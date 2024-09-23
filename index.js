36344e5d34204f453b3d8fde8f0c6a39cc39072f
CompSciHigh24 <csh-github@compscihigh.org>
2024-09-22 22:08:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // CLASSWORK: Functions Review - Regular Functions and Arrow Functions
 // Follow the instructions and complete each task.
+// Make sure to run your code using node index.js
+
+/*
+   ==== Make sure to run your code ====
+  use the command 'node index.js' in the terminal
+*/
 
 // TASK 1: Define a regular function to divide two numbers.
 // 1. Define a function named `divide` that takes two inputs: num1 and num2.
@@ -27,7 +33,11 @@
 // 2. Inside the arrow function, return the result of squaring the number (num * num).
 // 3. Call the `square` function with the argument 6, and log the result.
 
-
+/*
+   ==== Final Commit Instructions ====
+   At this point, commit your work with an appropriate commit message.
+   Sync changes to push your commits and submit the assignment!
+*/
 
 // BONUS TASK: 
 // Write a new function that within calls any 2 of the above functions and returns the sum or concatenation of their output. 
